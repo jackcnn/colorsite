929496383b4ff8a690efab655252344619850633
陆荣泽-菲拓公司 <1946755280@qq.com>
2017-08-31 18:38:06 +0800

    commit

---
+++ common/config/main-local.php
@@ -0,0 +1,33 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=120.77.181.89;dbname=friends',
+            'username' => 'root',
+            'password' => 'lrongZe*+-/214',
+            'charset' => 'utf8',
+            'tablePrefix' => 'friends_',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.qq.com',
+                'username' => '1946755280@qq.com',
+                'password' => 'SA523BER1',
+                'port' => '25',
+                'encryption' => 'tls',
+            ],
+            'messageConfig'=>[
+                'charset'=>'UTF-8',
+                'from'=>['1946755280@qq.com'=>'SUPER_ADMIN']
+            ],
+        ],
+    ],
+];
