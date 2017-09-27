36e824037ba5380cc2e2790600f8a3d4a1dadfc3
陆荣泽-菲拓公司 <1946755280@qq.com>
2017-09-27 18:17:25 +0800

    微信认证

--- frontend/widgets/views/jweixin.php
+++ frontend/widgets/views/jweixin.php
@@ -34,17 +34,15 @@ wx.onMenuShareTimeline({
 });
 
 wx.onMenuShareAppMessage({
-    title: '自定义分享', // 分享标题
-    desc: '自定义分享的描述', // 分享描述
-    link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-    imgUrl: 'https://326108993.com/uploads/00039/201709/57bddbc0f4987815c5ad93d4c3ed722b.jpg', // 分享图标
-    type: 'link', // 分享类型,music、video或link，不填默认为link
-    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+    title: '自定义分享',
+    desc: '自定义分享的描述',
+    link: location.href,
+    imgUrl: 'https://326108993.com/uploads/00039/201709/57bddbc0f4987815c5ad93d4c3ed722b.jpg',
     success: function () { 
-        // 用户确认分享后执行的回调函数
+       
     },
     cancel: function () { 
-        // 用户取消分享后执行的回调函数
+        
     }
 });
 
