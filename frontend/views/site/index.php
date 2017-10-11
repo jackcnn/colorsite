773ed8c3ed5785c0a0242f98479643f633031cac
lurongze <1946755280@qq.com>
2017-10-11 20:41:33 +0800

    菜品

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -67,12 +67,10 @@
                                                     <span class="oldPrice" <?php if($v['oprice']<1){?>style="display: none;"<?php }?>>￥<?=$v['oprice']/100?></span></div>
                                                 <div class="cartcontrol-wrapper">
                                                     <div class="cartcontrol">
-                                                        <div class="cart-decrease">
+                                                        <div class="cart-decrease" style="display: none;">
                                                             <span class="icon-remove_circle_outline inner"></span>
                                                         </div>
-                                                        <div class="cart-count">
-                                                            1
-                                                        </div>
+                                                        <div class="cart-count" data-price = "<?=$v['price']?>" style="display: none;">0</div>
                                                         <div class="cart-add"><i class="icon-add_circle"></i></div>
                                                     </div>
                                                 </div>
@@ -91,6 +89,49 @@
 
             </div>
 
+
+            <div>
+                <div class="shopCart">
+                    <div class="content">
+                        <div class="content-left">
+                            <div class="logo-wrapper">
+                                <div class="badge">
+                                    0
+                                </div>
+                                <div class="logo active"><i class="icon-shopping_cart"></i></div>
+                            </div>
+                            <div class="price active">
+                                ￥0
+                            </div>
+                            <div class="desc">
+                                订单描述
+                            </div>
+                        </div>
+                        <div class="content-right">去结算</div>
+                    </div>
+
+                    <div class="shopcart-list" style="display: none;">
+                        <div class="list-header"><h1 class="title">购物车</h1> <span class="empty">清空</span></div>
+                        <div class="list-content">
+                            <ul style="transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1); transition-duration: 0ms; transform: translate(0px, 0px) translateZ(0px);">
+                                <li class="food"><span class="name">皮蛋瘦肉粥</span>
+                                    <div class="price"><span data-v-3bb4d644="">￥10</span></div>
+                                    <div class="cartcontrol-wrapper">
+                                        <div class="cartcontrol">
+                                            <div class="cart-decrease"><span class="icon-remove_circle_outline inner"></span></div>
+                                            <div class="cart-count">
+                                                0
+                                            </div>
+                                            <div class="cart-add"><i class="icon-add_circle"></i></div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
 
@@ -100,29 +141,72 @@
 </body>
 <script>
 $(function () {
-
     var list = new Array();
-
     $(".foods_category").each(function () {
         list.push($(this).offset().top)
     })
-    var tt=0;
-    console.log(list[tt]);
-
-
     $(".menu_item").click(function () {
         var self = $(this);
         if(!self.hasClass("menu_item_selected")){
             $(".menu_item").removeClass("menu_item_selected");
             self.addClass("menu_item_selected");
-
             var index = $(".category_h1_"+self.data("id")).data("key");
+            $(".foods").animate({scrollTop: parseInt(list[index])-217 }, {duration: 300,easing: "swing"});
+        }
+    })
 
-
-            $(".foods").animate({scrollTop: parseInt(list[index])-217 }, {duration: 500,easing: "swing"});
+    //购物车相关
+    $(".cart-add").click(function () {
+        var self = $(this);
+        var target = self.parent().find(".cart-count");
+        var res = parseInt(target.html())+1;
+        target.html(res);
+        if(res > 0){
+            self.parent().find(".cart-decrease").show();
+            target.show();
         }
 
+        get_result();
     })
+
+    $(".cart-decrease").click(function () {
+        var self = $(this);
+        var target = self.parent().find(".cart-count");
+        var res = parseInt(target.html())-1;
+        target.html(res);
+        if(res < 1){
+            self.hide();
+            target.hide();
+        }
+
+        get_result();
+
+    })
+
+
+
+
+
+    function get_result()
+    {
+        var count = 0;
+        var total = 0;
+        $(".foods_category .cart-count").each(function () {
+            count = parseInt($(this).html()) + count ;
+
+            total = parseInt($(this).data("price")*parseInt($(this).html()))+total;
+        })
+
+        $(".shopCart .badge").html(count);
+        $(".shopCart .price").html("￥"+total/100);
+
+    }
+
+
+
+
+
+
 })
 </script>
 </html>
