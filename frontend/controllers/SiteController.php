b72fa79dfc20e4def1fa361520d7410dfd1a7d55
lurongze <1946755280@qq.com>
2017-10-11 16:32:06 +0800

    菜品

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -12,7 +12,7 @@ class SiteController extends BaseController
     public function actionIndex()
     {
 
-        $category = Category::find()->where(['ownerid'=>$this->ownerid])->asArray()->orderBy("sort,id")->all();
+        $category = Category::find()->where(['ownerid'=>$this->ownerid,'table'=>'restaurant'])->asArray()->orderBy("sort,id")->all();
 
         $store = Stores::find()->where(['ownerid'=>$this->ownerid,'id'=>\Yii::$app->request->get("stid")])->asArray()->one();
 
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -25,33 +25,65 @@
             <a>评论</a>
         </div>
     </div>
-
     <div>
         <div class="container">
-            <div class="menu_box">
-
+            <div class="menu_wrapper">
                 <div class="menu">
-                    <ul class="menu_wrapper" style="transition-timing-function: cubic-bezier(0.165, 0.84, 0.44, 1); transition-duration: 0ms; transform: translate(0px, 0px) translateZ(0px);">
-                        <?php for($i=0;$i<30;$i++){?>
+                    <ul class="menu_box">
+                        <?php foreach($category as $key=>$value){?>
                             <li class="menu_item">
-
-                                <span class="text">热卖单品热卖单品</span>
-
+                                <span class="text"><?=$value['name']?></span>
                             </li>
                         <?php }?>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="foods_wrapper">
+                <div class="foods">
+
+                    <ul class="foods_box">
+
+                        <li class="foods_category">
+                            <h1>主食</h1>
+                            <ul>
+                                <li class="foods_item">
+                                    <div class="icon">
+                                        <img width="57" height="57" src="http://fuss10.elemecdn.com/d/2d/b1eb45b305635d9dd04ddf157165fjpeg.jpeg?imageView2/1/w/114/h/114">
+                                    </div>
+                                    <div class="content">
+                                        <h2>娃娃菜炖豆腐</h2>
+                                        <p class="description">新鲜娃娃菜加新鲜豆腐</p>
+                                        <div class="sell-info">
+                                            <span class="sellCount">月售43份</span>
+                                            <span class="rating">好评率92%</span>
+                                        </div>
+                                        <div class="price">
+                                            <span class="newPrice"><span class="unit">￥</span>17</span>
+                                            <span class="oldPrice">￥20</span></div>
+                                        <div class="cartcontrol-wrapper">
+                                            <div class="cartcontrol">
+                                                <div class="cart-decrease">
+                                                    <span class="icon-remove_circle_outline inner"></span>
+                                                </div>
+                                                <div class="cart-count">
+                                                    1
+                                                </div>
+                                                <div class="cart-add"><i class="icon-add_circle"></i></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
 
                     </ul>
 
                 </div>
 
-
             </div>
 
-
         </div>
-
-
-
 
 
     </div>
