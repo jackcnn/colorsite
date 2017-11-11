c96c6a1106f555aece6f24c6d05ea81bca4866be
lurongze <1946755280@qq.com>
2017-11-11 19:49:48 +0800

    淘宝客

---
+++ frontend/modules/tbk/TbkMoudle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\tbk;
+
+/**
+ * tbk module definition class
+ */
+class TbkMoudle extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'frontend\modules\tbk\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
