c98195f4d6ed233afa03a9695d941730016d3dda
1946755280@qq.com <SA523BERxia$meng>
2017-11-24 17:34:30 +0800

    报名

--- frontend/modules/dish/controllers/IndexController.php
+++ frontend/modules/dish/controllers/IndexController.php
@@ -456,9 +456,9 @@ class IndexController extends BaseController
             $input->SetSubAppid($this->mini_appid);//这里使用小程序appid
             $input->SetSubMch_id($mchInfo['mch_number']);
             $input->SetSubOpenid($openid);
-            $orderRes = \common\weixin\WxPayHelper::unifiedOrder($input , $mchInfo['mch_key']);
+            $orderRes = \common\weixin\WxPayHelper::unifiedOrder($input , $this->mchkey);
             $asJson['res'] = $orderRes;
-            $asJson['jsapiparams'] = \common\weixin\WxPayHelper::GetJsApiParameters($orderRes , $mchInfo['mch_key']);
+            $asJson['jsapiparams'] = \common\weixin\WxPayHelper::GetJsApiParameters($orderRes , $this->mchkey);
         }catch (\Exception $e){
             $asJson['success'] = false;
             $asJson['msg'] = $e->getMessage();
