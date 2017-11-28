0b1e100eafb3cf7a8200fcc09f6308a7dfc0d445
1946755280@qq.com <SA523BERxia$meng>
2017-11-28 13:09:38 +0800

    报名

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -1,11 +1,20 @@
 <?php
 return [
     'components' => [
+//        'db' => [  阿里云的数据库
+//            'class' => 'yii\db\Connection',
+//            'dsn' => 'mysql:host=120.77.181.89;dbname=colorsite',
+//            'username' => 'root',
+//            'password' => 'lrongZe*+-/214',
+//            'charset' => 'utf8',
+//            'tablePrefix' => 'cs_',
+//        ],
+        //腾讯云的数据库
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=120.77.181.89;dbname=colorsite',
+            'dsn' => 'mysql:host=gz-cdb-c15b3wfx.sql.tencentcdb.com:63606;dbname=colorsite',
             'username' => 'root',
-            'password' => 'lrongZe*+-/214',
+            'password' => 'lrongZe*+-214',
             'charset' => 'utf8',
             'tablePrefix' => 'cs_',
         ],
